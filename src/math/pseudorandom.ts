4e4ec839cf37b5e1926a631df1743370d0519a3f
Jerry Wang <x.jerry.wang@gmail.com>
2023-01-16 17:37:19 +0800

    feat: implement xorshift algorithm

--- src/math/pseudorandom.ts
+++ src/math/pseudorandom.ts
@@ -1,27 +1,31 @@
-/**
- * https://www.wikiwand.com/en/Pseudorandom_number_generator#/Implementation
- *
- * Pseudo random
- *
- * @example
- * ```ts
- * const random = Random(10)
- * random() // => 0.9417890602847239
- * random() // => 0.722160604293402
- *
- * const random2 = Random(-10)
- * random2() // => 0.9417890602847239
- * ```
- *
- * @param seed
- */
-export function Random(seed = Date.now()) {
-  seed = Math.abs(seed)
+class Xorshift {
+  private x: number
+  private y: number
+  private z: number
+  private w: number
+
+  constructor(seed: number = Date.now()) {
+    this.x = seed
+    this.y = 362436069
+    this.z = 521288629
+    this.w = 88675121
+  }
+
+  next() {
+    let t = this.x ^ (this.x << 11)
+    this.x = this.y
+    this.y = this.z
+    this.z = this.w
+    this.w = this.w ^ (this.w >> 19) ^ (t ^ (t >> 8))
+    return this.w / 2 ** 31 // 2^32-1
+  }
+}
+
+export function Random(seed = 0) {
+  const x = new Xorshift(seed)
 
   return (min = 0, max = 1) => {
-    const a = seed * 15485863
-    seed++
-    const r = ((a * a * a) % 2038074743) / 2038074743
+    const r = x.next()
 
     return min + (max - min) * r
   }
