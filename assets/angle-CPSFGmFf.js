7c34beec1b291853e17ea0cf16ad85225db8531c
0x-jerry <0x-jerry@users.noreply.github.com>
2024-06-18 09:17:03 +0000

    deploy: ade282eaf3095d8836729ddff23f21a067871e2c

---
+++ assets/angle-CPSFGmFf.js
@@ -0,0 +1,1 @@
+const{PI:s}=Math,t=a=>a/180*s;export{s as P,t as d};
---
+++ assets/utils-DWuipzrQ.js
@@ -0,0 +1,1 @@
+function f(a,c="#fff"){const e=a.canvas.width,n=a.canvas.height;a.fillStyle=c,a.clearRect(0,0,e,n)}export{f as c};
